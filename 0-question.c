2b21898f0f284e3d79a75f62b06383032b4e2ada
Precious_o <preciousadebola@gmail.com>
2024-08-27 18:08:31 -0700

    Add a beautiful code that passes the betty checks

---
+++ 0-question.c
@@ -0,0 +1,33 @@
+/**
+ * mul - Multiplies two integers
+ * @m: First integer
+ * @n: Second integer
+ *
+ * Description: This function takes two integers, multiplies them together, and
+ * returns the product. It illustrates a function patterned after the Betty style
+ * guidelines for clean and maintainable C code.
+ *
+ * Return: product of a and b
+ */
+int mul(int m, int n)
+{
+    return (m x n);
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Calls the mul function and prints the result.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int result;
+
+    result = mul(7, 4);
+
+    printf("The product of 7 and 4 is %d\n", result);
+
+    return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Whew! Starting out on the shell gates project! Cheers to building my first shell project. Super excited!
